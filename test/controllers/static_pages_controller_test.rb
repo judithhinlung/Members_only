3e5db6079593b78adc36a457efe1812702c7fdd4
judithhinlung <judithhin@gmail.com>
2019-05-09 15:33:19 -0700

    Update StaticPages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,16 +2,17 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get root_url
+    get static_pages_home_url
     assert_response :success
   end
+
   test "should get about" do
-    get about_path
+    get static_pages_about_url
     assert_response :success
   end
 
   test "should get contact" do
-    get contact_path
+    get static_pages_contact_url
     assert_response :success
   end
 
